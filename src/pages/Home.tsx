a8092d602f991cdb744e1fde6dd559c5a4f91401
mgsainyinyitun <mgsainyinyitun.tumdy@gmail.com>
2024-12-07 15:47:03 +0630

    Innital commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Hero } from '../components/home/Hero';
 import { FeaturedProducts } from '../components/home/FeaturedProducts';
 import { ServicesShowcase } from '../components/home/ServicesShowcase';
